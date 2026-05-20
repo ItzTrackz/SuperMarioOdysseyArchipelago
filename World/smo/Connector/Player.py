8f1861ad959e9023bfd4676f87a39fea019e2955
Kgamer77 <147015128+Kgamer77@users.noreply.github.com>
2026-05-20 14:32:00 -0700

    Update Player.py
    Added regional coin tracking data

--- World/smo/Connector/Player.py
+++ World/smo/Connector/Player.py
@@ -1,5 +1,8 @@
 from NetUtils import NetworkItem
-from .Data import moon_list, id_to_name, goals
+from .Data import moon_list, id_to_name, goals, worlds
+from ..Data.EntranceData import SMOEntranceData
+from ..Locations import regional_coins, regional_coin_groups, regional_sub_area_to_kingdom
+from ..Data.ItemData import SMOItemData
 
 class SMOPlayer:
     MAX_MOONS = {
@@ -24,6 +27,22 @@ class SMOPlayer:
         "Mushroom Multi-Moon": 5,
         "Dark Side Multi-Moon": 1,
         "Darker Side Multi-Moon": 1
+    }
+
+    MAX_REGIONAL_COINS = {
+        "Cap Kingdom Regional Coin" : 50,
+        "Cascade Kingdom Regional Coin" : 50,
+        "Sand Kingdom Regional Coin" : 100,
+        "Wooded Kingdom Regional Coin" : 100,
+        "Lake Kingdom Regional Coin" : 50,
+        "Lost Kingdom Regional Coin" : 50,
+        "Metro Kingdom Regional Coin" : 100,
+        "Seaside Kingdom Regional Coin" : 100,
+        "Snow Kingdom Regional Coin" : 50,
+        "Luncheon Kingdom Regional Coin" : 100,
+        "Bowser's Kingdom Regional Coin" : 100,
+        "Moon Kingdom Regional Coin" : 50,
+        "Mushroom Kingdom Regional Coin" : 100,
     }
     def __init__(self):
         self.moons = {
@@ -66,6 +85,24 @@ class SMOPlayer:
         "Darker Side Multi-Moon": 0,
         "Beat the Game": -1
         }
+
+        self.regional_coins = {
+            "Cap Kingdom Regional Coin": 0,
+            "Cascade Kingdom Regional Coin": 0,
+            "Sand Kingdom Regional Coin": 0,
+            "Wooded Kingdom Regional Coin": 0,
+            "Lake Kingdom Regional Coin": 0,
+            "Lost Kingdom Regional Coin": 0,
+            "Metro Kingdom Regional Coin": 0,
+            "Seaside Kingdom Regional Coin": 0,
+            "Snow Kingdom Regional Coin": 0,
+            "Luncheon Kingdom Regional Coin": 0,
+            "Bowser's Kingdom Regional Coin": 0,
+            "Moon Kingdom Regional Coin": 0,
+            "Mushroom Kingdom Regional Coin": 0,
+        }
+
+        self.regional_group_progress = []
 
         self.messages : list[str] = []
         self.MAX_MESSAGE_SIZE = 0x42
@@ -134,6 +171,23 @@ class SMOPlayer:
             "Beat the Game": -1
         }
 
+    def reset_regional_coins(self):
+        self.regional_coins = {
+            "Cap Kingdom Regional Coin": 0,
+            "Cascade Kingdom Regional Coin": 0,
+            "Sand Kingdom Regional Coin": 0,
+            "Wooded Kingdom Regional Coin": 0,
+            "Lake Kingdom Regional Coin": 0,
+            "Lost Kingdom Regional Coin": 0,
+            "Metro Kingdom Regional Coin": 0,
+            "Seaside Kingdom Regional Coin": 0,
+            "Snow Kingdom Regional Coin": 0,
+            "Luncheon Kingdom Regional Coin": 0,
+            "Bowser's Kingdom Regional Coin": 0,
+            "Moon Kingdom Regional Coin": 0,
+            "Mushroom Kingdom Regional Coin": 0,
+        }
+
     def get_next_moon(self, item : int) -> int:
         """
         Args:
@@ -153,6 +207,72 @@ class SMOPlayer:
         self.moons[item_name] += 1
         return moon_id
 
+    def get_next_regional_coin(self, item: int) -> tuple[str, str, int] | str:
+        """
+            Args:
+                item: id of the respective Archipelago Item
+            Returns:
+                next Regional Coin (placement_id, stage_name, world_id) to send to SMO
+        """
+        item_name : str = id_to_name[item]
+
+        if self.regional_coins[item_name] >= self.MAX_REGIONAL_COINS[item_name]:
+            return "", "", -1
+
+        placement_id: str = ""
+        stage_name: str = ""
+        world_id: int = -1
+
+        kingdom = item_name.split()[0]
+        world_id = worlds[kingdom]
+        stages = [kingdom + " Kingdom"]
+        stages += regional_sub_area_to_kingdom[
+                      kingdom.lower()] if kingdom.lower() in regional_sub_area_to_kingdom else []
+
+        regional_ids = []
+        for stage in stages:
+            stage_name = SMOEntranceData.display_name_to_internal_name[
+                stage.replace(" Regional Coins", "").replace(" Regional Groups","")]
+            for group in regional_coin_groups[stage_name]:
+                regional_ids += regional_coin_groups[stage_name][group]
+            if len(regional_ids) > self.regional_coins[item_name]:
+                break
+
+        if len(regional_ids) <= self.regional_coins[item_name]:
+            return f"{kingdom} {len(regional_ids)} {regional_ids}"
+        location_id = regional_ids[self.regional_coins[item_name]]
+        for coin in regional_coins[stage_name]:
+            if regional_coins[stage_name][coin] == location_id:
+                placement_id = coin
+                break
+        self.regional_coins[item_name] += 1
+        return placement_id, stage_name, world_id
+
+
+    def get_regional_group(self, stage: str, location_id: int) -> int:
+        """
+            Args:
+                stage: The name of the stage the Regional Coin is from
+                location_id: The id of the Regional Coin location
+            Returns:
+                complete Regional Coin Group location id
+        """
+        stage = stage.strip()
+        self.regional_group_progress.append(location_id)
+        for group in regional_coin_groups[stage]:
+            progress : int = 0
+            for coin in regional_coin_groups[stage][group]:
+                if coin in self.regional_group_progress:
+                    progress += 1
+            if progress == len(regional_coin_groups[stage][group]):
+                for coin in regional_coin_groups[stage][group]:
+                    self.regional_group_progress.remove(coin)
+                return group
+
+        return -1
+
+
+
     def add_message(self, message : str) -> None:
         """
         Adds message to the player's messages list automatically subdividing the message to fit in a single ChatMessagePacket.
@@ -169,30 +289,28 @@ class SMOPlayer:
         else:
             message_parts = message.split()
             current_message : str = message_parts.pop(0)
+            current_messages : list[str] = []
             while len(message_parts) > 0:
                 if len(message_parts[0]) + len(current_message) < self.MAX_MESSAGE_SIZE:
                     current_message += f" {message_parts.pop(0)}"
                 else:
                     if len(current_message.replace("\t", "")) > 0:
-                        self.messages.append(current_message)
+                        current_messages.append(current_message)
                         current_message = "\t"
                     if len(message_parts[0]) > self.MAX_MESSAGE_SIZE:
                         cut_part = message_parts[0][0:self.MAX_MESSAGE_SIZE - 2] + "-"
                         current_message += cut_part
                         message_parts[0] = message_parts[0][self.MAX_MESSAGE_SIZE - 2:]
 
+            # Make two part message appear in order
+            while len(current_messages) > 0:
+                self.messages.append(current_messages.pop(-1))
             self.messages.append(current_message)
 
-    def next_messages(self) -> list[str]:
-        match len(self.messages):
-            case 0:
-                return ["", "", ""]
-            case 1:
-                return ["", "", self.messages.pop(0)]
-            case 2:
-                return ["", self.messages.pop(0), self.messages[0]]
-            case _:
-                return [self.messages.pop(0), self.messages[0], self.messages[1]]
+    def next_messages(self) -> str:
+        if len(self.messages) >= 1:
+            return self.messages.pop(0)
+        return ""
 
     def check_goal(self, location : int) -> bool:
         if self.goal and self.goal in goals:
